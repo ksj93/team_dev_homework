4f97a4d93e8fb101cd4f6b08e0054a5e62743f52
ksj <ksjee17@gmail.com>
2021-11-11 11:07:55 +0000

    211111_try_issues2 #2

--- app/mailers/assign_mailer.rb
+++ app/mailers/assign_mailer.rb
@@ -1,9 +1,16 @@
 class AssignMailer < ApplicationMailer
-  default from: 'from@example.com'
+  default from: 'ksjee17@gmail.com'
+
 
   def assign_mail(email, password)
     @email = email
     @password = password
     mail to: @email, subject: I18n.t('views.messages.complete_registration')
   end
+  def appoint_mail(assign)
+    @assign = assign
+    @team_info = Team.where(owner_id:@assign.id)
+    mail to: @assign.email, subject: "あなたはチームのリーダーになりました"
+  end
+
 end
